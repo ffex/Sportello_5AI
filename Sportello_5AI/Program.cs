8e89d9b51cca0bafe91190e211e75b322ca8e702
ffex <kurogan21@gmail.com>
2021-03-26 11:55:55 +0100

    Initial commit

---
+++ Sportello_5AI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Sportello_5AI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* configurazione server */
+            Socket Server;
+            Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = IPAddress.Any;
+
+            // config: devo configurare l'EndPoint
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+
+            Server.Bind(ipep);
+            /* fine configurazione server */
+        }
+    }
+}
